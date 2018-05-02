6f3a7129afd32f7025b5c979bbc24d0a86962385
derdelt <erd@conceptual.ca>
2018-05-02 12:57:36 -0400

    fixed(ish) percent healing on older replays

--- src/lib/replay/analyzers/builtin/ScoreAnalyser.ts
+++ src/lib/replay/analyzers/builtin/ScoreAnalyser.ts
@@ -23,6 +23,45 @@ export interface ISimplePlayerScoreStats {
     "TeamLevel": number;
 }
 
+export interface IPlayerScoreStats {
+    "Takedowns": number;
+    "Deaths": number;
+    "SoloKill": number;
+    "Assists": number;
+    "MetaExperience": number;
+    "ExperienceContribution": number;
+    "Healing": number;
+    "SiegeDamage": number;
+    "HeroDamage": number;
+    "DamageTaken": number;
+    "TeamLevel": number;
+    "MercCampCaptures": number;
+    "WatchTowerCaptures": number;
+    "SelfHealing": number;
+    "TimeSpentDead": number;
+    "TimeCCdEnemyHeroes": number;
+    "CreepDamage": number;
+    "SummonDamage": number;
+    "DamageSoaked": number;
+    "HighestKillStreak": number;
+    "ProtectionGivenToAllies": number;
+    "TimeSilencingEnemyHeroes": number;
+    "TimeRootingEnemyHeroes": number;
+    "ClutchHealsPerformed": number;
+    "EscapesPerformed": number;
+    "VengeancesPerformed": number;
+    "TeamfightEscapesPerformed": number;
+    "OutnumberedDeaths": number;
+    "OnFireTimeOnFire": number;
+    "TeamfightDamageTaken": number;
+    "TimeOnPoint": number;
+    "TeamfightHealingDone": number;
+    // map specific
+    "DamageDoneToImmortal"?: number;
+    "DamageDoneToShrineMinions"?: number;
+
+}
+
 export interface ISimplePlayerScore {
     hero: string;
     name: string;
@@ -30,8 +69,18 @@ export interface ISimplePlayerScore {
     won: boolean;
     hasChatSilence: boolean;
     hasVoiceSilence: boolean;
-    stats:ISimplePlayerScoreStats;
+    stats: ISimplePlayerScoreStats;
     awards: string[];
+}
+
+export interface IPlayerScore {
+    hero: string;
+    name: string;
+    team: number;
+    won: boolean;
+    hasChatSilence: boolean;
+    hasVoiceSilence: boolean;
+    stats: ISimplePlayerScoreStats;
 }
 
 
@@ -63,7 +112,7 @@ export class ScoreAnalyser extends AbstractReplayAnalyser {
     public get scoreScreenData(): Promise<IScoreScreenData> {
         return (async (): Promise<IScoreScreenData> => {
             const playerScores = await this.playerScoresSimple;
-            
+
             const playerScoresQ = linq.from(playerScores);
             const team1Kills = playerScoresQ.where(_ => _.team === 1).sum(_ => _.stats.Deaths);
             const team2Kills = playerScoresQ.where(_ => _.team === 0).sum(_ => _.stats.Deaths);
@@ -148,14 +197,96 @@ export class ScoreAnalyser extends AbstractReplayAnalyser {
                     .where(_ => _.value != undefined)
                     .join(playerScoresQ, (s) => s.slot, p => p.slot, (s, p) => {
                         //p.scores[statName] = s.value;
-                        if(s.value === 1){
-                           p.awards.push(awardName);
+                        if (s.value === 1) {
+                            p.awards.push(awardName);
                         }
                         return p;
                     }).toArray();
             }
-           
+
+
+            const tmp = await this.playerScoresFull;
+            console.log('playerScoresFull', tmp);
             return playerScores;
+        })();
+    }
+
+    @RunOnWorker()
+    @RequiredReplayVersion(40336, 'Player score data not supported by this version of replay')
+    public get playerScoresFull(): Promise<IPlayerScore[]> {
+        return (async (): Promise<any> => {
+            const trackerQueriable = await this.trackerEventsQueriable;
+            const players = await this.basicReplayAnalyser.playerList;
+
+            const stats = <ISScoreResultEvent><any>trackerQueriable.where(_ => isSScoreResultEvent(_)).last();
+
+            const playerStats: IPlayerScore[] = [];
+            for (let i = 0; i < players.length; i++) {
+                const player = players[i];
+                const playerScore: IPlayerScore = {
+                    name: player.name,
+                    hero: player.hero,
+                    team: player.team,
+                    won: player.won,
+                    hasChatSilence: player.hasChatSilence,
+                    hasVoiceSilence: player.hasVoiceSilence,
+                    stats: <IPlayerScoreStats>{}
+                };
+
+                for (let j = 0; j < stats.m_instanceList.length; j++) {
+                    const stat = stats.m_instanceList[j];
+                    if (
+                        !stat.m_name.endsWith('Boolean') &&
+                        !stat.m_name.endsWith('Talent') &&
+                        !stat.m_name.startsWith('Plays') &&
+                        !stat.m_name.startsWith('Wins') &&
+                        !stat.m_name.startsWith('TeamWins') &&
+                        !stat.m_name.endsWith('Level') &&
+                        stat.m_name !== 'Role' &&
+                        stat.m_name !== 'GameScore'
+                    ) {
+                        const pStats = stat.m_values[player.slot];
+                        if (Array.isArray(pStats) && pStats.length) {
+                            const pStat = pStats[0];
+                            playerScore.stats[stat.m_name] = pStat.m_value;
+                        }
+                    }
+                }
+                playerStats.push(playerScore);
+            }
+            const playerStatsQ = linq.from(playerStats);
+
+            const teamTakedowns = [
+                playerStatsQ.where(_ => _.team === 1).sum(_ => _.stats.Deaths),
+                playerStatsQ.where(_ => _.team === 0).sum(_ => _.stats.Deaths)
+            ];
+
+            const teamDamageTaken = [
+                playerStatsQ.where(_ => _.team === 0).sum(_ => _.stats.DamageTaken),
+                playerStatsQ.where(_ => _.team === 1).sum(_ => _.stats.DamageTaken),
+            ];
+
+            const teamHeroDamageAgainst = [
+                playerStatsQ.where(_ => _.team === 1).sum(_ => _.stats.HeroDamage),
+                playerStatsQ.where(_ => _.team === 0).sum(_ => _.stats.HeroDamage),
+            ];
+           
+            for (let i = 0; i < playerStats.length; i++) {
+                const player = playerStats[i];
+                const pStats = player.stats;
+                pStats['KillParticipation'] = pStats.Takedowns / teamTakedowns[player.team];
+                pStats['AverageDamageTakenPerLife'] = pStats.DamageTaken / (pStats.Deaths + 1);
+                pStats['KDARatio'] = pStats.Takedowns / (pStats.Deaths + 1);
+                pStats['KDRatio'] = pStats.SoloKill / (pStats.Deaths + 1);
+                pStats['ADRatio'] = pStats.Assists / (pStats.Deaths + 1);
+                pStats['PercentDamageHealed'] = pStats.Healing / (teamDamageTaken[player.team] || 1);
+                // older replays only record damage taken for warriors // flacky fix wont be accurate as hero damage is from heroes only and damage taken is from all sources
+                if(this.versionMatches('<63507')){
+                    pStats['PercentDamageHealed'] = pStats.Healing / (teamHeroDamageAgainst[player.team] || 1);
+                }
+
+            }
+            return playerStats;
         })();
     }
 }
